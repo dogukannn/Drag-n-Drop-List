00e5bdcf2196407201fa14820239f5b17609613f
dogukannn <doguklan.ozurk@metu.edu.tr>
2020-06-26 15:41:09 +0300

    No change, gpg sign try-2

--- app/src/main/java/com/example/drag_drop_list/ExampleLevel.kt
+++ app/src/main/java/com/example/drag_drop_list/ExampleLevel.kt
@@ -20,7 +20,7 @@ class ExampleLevel() : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_example_level)
         supportActionBar?.hide()
-        //gpa sign try1
+        //gpa sign try2
         var bundle = this.intent.extras
         exampleList= intent.getSerializableExtra("value") as ArrayList<ExampleItem>
 
