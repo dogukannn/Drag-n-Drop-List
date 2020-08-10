7dac0af9a61e2141f2773acc27f4f3747890c0f7
dogukannn <dogukan.ozturk9@outlook.com>
2020-08-10 14:02:43 +0300

    Hint system implemented and various bug fixes.

--- app/src/main/java/com/example/drag_drop_list/RandomizedLevel.kt
+++ app/src/main/java/com/example/drag_drop_list/RandomizedLevel.kt
@@ -32,8 +32,9 @@ class RandomizedLevel : AppCompatActivity() {
     var buttonConf : Button? = null
     var buttonMast : Button? = null
     var buttonNext : Button? = null
-
+    var hints = ArrayList<Int>()
     val intnt = Intent()
+    var hint_limit = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,6 +57,7 @@ class RandomizedLevel : AppCompatActivity() {
         buttonMast = button11
         buttonNext = button12
 
+        hint_limit = (count - 1) / 2
 
         allItems.shuffle()
         countAll = allItems.size
@@ -95,9 +97,30 @@ class RandomizedLevel : AppCompatActivity() {
 
             val fromPosition = viewHolder.adapterPosition
             val toPosition = target.adapterPosition
+            if(fromPosition in hints){
+                return false
+            }
+            if(toPosition in hints)
+            {
 
-            Collections.swap(exampleList,fromPosition,toPosition)
-            recyclerView.adapter?.notifyItemMoved(fromPosition,toPosition)
+            }
+            else
+            {
+                Collections.swap(exampleList,fromPosition,toPosition)
+                //recyclerView.adapter?.notifyItemMoved(fromPosition,toPosition)
+
+                if(fromPosition > toPosition)
+                {
+                    recycler_view.adapter?.notifyItemMoved(toPosition, fromPosition);
+                    recycler_view.adapter?.notifyItemMoved(fromPosition-1, toPosition);
+                }
+                else if(toPosition > fromPosition)
+                {
+                    recycler_view.adapter?.notifyItemMoved(fromPosition, toPosition);
+                    recycler_view.adapter?.notifyItemMoved(toPosition-1, fromPosition);
+                }
+            }
+
 
             return false
         }
@@ -119,6 +142,10 @@ class RandomizedLevel : AppCompatActivity() {
     val itemTouchHelper = ItemTouchHelper(simpleCallback)
 
     fun startDragging(viewHolder: RecyclerView.ViewHolder) {
+        if(viewHolder.adapterPosition in hints)
+        {
+            return
+        }
         itemTouchHelper.startDrag(viewHolder)
     }
     fun checkLevel(v: View){
@@ -147,6 +174,10 @@ class RandomizedLevel : AppCompatActivity() {
             if(mastery < countAll *5){
                 mastery += count
             }
+            if (mastery > countAll * 5)
+            {
+                mastery = countAll * 5
+            }
             val mxd = ((mastery *75)/ (countAll*5))
 
             intnt.putExtra("mst",mastery)
@@ -168,6 +199,7 @@ class RandomizedLevel : AppCompatActivity() {
             intnt.putExtra("mst",mastery)
             setResult(categoryNumber,intnt)
 
+            exampleListOrdered.clear()
             for(x in 0..(count-1)){
                 exampleListOrdered.add(exampleList[x])
             }
@@ -206,6 +238,8 @@ class RandomizedLevel : AppCompatActivity() {
                     DialogInterface.OnClickListener { dialog, which ->
                         allItems.shuffle()
                         l = 0
+                        hint_limit = (count - 1) / 2
+                        hints.clear()
                         goNextLevel(v)
                     }) // A null listener allows the button to dismiss the dialog and take no further action.
                 .setNegativeButton("Go Back", {dialog, which ->
@@ -216,7 +250,8 @@ class RandomizedLevel : AppCompatActivity() {
 
             return
         }
-
+        hints.clear()
+        hint_limit = (count - 1) / 2
         exampleList.clear()
         for (x in 1..((count+1)/2) )
         {
@@ -235,20 +270,6 @@ class RandomizedLevel : AppCompatActivity() {
             b.add(a)
         }
         b.clear()
-        /*exampleList.add(allItems[l])
-        exampleList.add(allItems[l+1])
-        exampleList.add(allItems[l+2])
-        var a = (0..(count-1)).random()
-        while((a == l) || (a == (l+1)) || (a == (l+2))){
-            a = (0..(count-1)).random()
-        }
-        var b = (0..(count-1)).random()
-        while((b == l) || (b == (l+1)) || (b == (l+2)) || (b == a)){
-            b = (0..(count-1)).random()
-        }
-        exampleList.add(allItems[a])
-        exampleList.add(allItems[b])
-        l += 3*/
 
         recycler_view.adapter = RandomizedLevelAdapter(exampleList,act)
         buttonMast?.visibility = View.INVISIBLE
@@ -256,6 +277,66 @@ class RandomizedLevel : AppCompatActivity() {
 
         buttonHint?.visibility = View.VISIBLE
         buttonConf?.visibility = View.VISIBLE
+    }
+
+    fun hint(v: View) {
+        if (hint_limit == 0)
+        {
+            AlertDialog.Builder(this)
+                .setTitle("Ooops!")
+                .setMessage("You have used all of your hints.") // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes,
+                    DialogInterface.OnClickListener { dialog, which ->
+
+                    }) // A null listener allows the button to dismiss the dialog and take no further action.
+                .setIcon(R.drawable.ic_remove_circle_outline_black_24dp)
+                .show()
+
+            return
+        }
+        var rnd = 0
+
+        exampleListOrdered.clear()
+        for(x in 0..(count-1)){
+            exampleListOrdered.add(exampleList[x])
+        }
+        exampleListOrdered.sortBy { it.text2 }
+
+
+        rnd = (0 until count).random()
+        while(rnd  in hints){
+            rnd = (0 until count).random()
+        }
+
+        var i = 0
+        while(i < count)
+        {
+            if(exampleList[rnd] == exampleListOrdered[i])
+            {
+                break
+            }
+            i++
+        }
+        hints.add(i)
+
+        Collections.swap(exampleList,rnd,i)
+
+        if(rnd > i)
+        {
+            recycler_view.adapter?.notifyItemMoved(i, rnd);
+            recycler_view.adapter?.notifyItemMoved(rnd-1, i);
+        }
+        else if(i > rnd)
+        {
+            recycler_view.adapter?.notifyItemMoved(rnd, i);
+            recycler_view.adapter?.notifyItemMoved(i-1, rnd);
+        }
+
+
+        recycler_view.layoutManager?.findViewByPosition(rnd)?.setBackgroundColor(Color.rgb(119,235,30))
+        i = 0
+        hint_limit--
     }
 
 
